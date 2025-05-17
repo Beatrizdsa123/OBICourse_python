b09afc77a21f04bcc36033cea74ef6662c448715
BeatrizAguiar-Dev <beatrizdasilvaaguiar4@gmail.com>
2025-05-17 17:02:37 -0300

    feat: new files until class 7

---
+++ homeCourse/home1/file1.py
@@ -0,0 +1,24 @@
+#4 aula 
+print('Olá mundo') #texto entre parentesis
+print(7 + 4) #numeros sem parenteses
+print('7' + '4') #aqui e uma soma de informacoes
+#print('ola' + 6) erro pois nao pode misturar
+
+#sem aspas sem acento e minusculo, variavel e um objeto
+
+nome = 'Guanabara' #variavel, recebe, valores
+idade = 25
+peso = 75.5
+print(nome, idade, peso)
+
+#variavel e um espaco guardado na memoria
+
+#INTERATIVO
+# input = leia
+
+nome2 = input('Qual o seu nome: ')
+idade2 = input('Qual sua idade: ')
+peso2 =input('Qual seu peso: ')
+
+print(nome2, idade2, peso2)
+
---
+++ homeCourse/home1/file2.py
@@ -0,0 +1,16 @@
+#1 desafio
+nome = input('Qual o seu nome: ')
+print('Seja bem vindo', nome, 'prazer em te conhecer')
+
+#2 desafio
+dia = input('Que dia você nasceu: ')
+mes = input('Que mês você nasceu: ')
+ano = input('Que ano você nasceu: ')
+
+print('Você nasceu no dia ', dia, ' de ' , mes, ' de ' , ano)
+
+#3 desafio
+num1 = int(input('1 número: ')) #usar int para dizer que e numero
+num2 = int(input('2 número: '))
+
+print(num1 + num2)
